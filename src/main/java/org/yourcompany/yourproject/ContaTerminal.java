e0f32c6d5b3ca5ca2daaad74d3e4bbf94a06c669
Francisco Araujo <francisco.br65190@Gmail.com>
2026-01-05 18:09:20 -0400

    pronto

--- src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -2,15 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package org.yourcompany.yourproject;
 
+
+package org.yourcompany.yourproject;
+import java.util.Scanner;
 /**
  *
  * @author misterfran
- */
+ */ 
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (Scanner Sc = new Scanner(System.in)) {
+            System.out.println("Olá, bem vindo ao banco Flarinthias, digite os dados da sua conta abaixo:");
+            System.out.println("Agência:");
+            String agencia = Sc.next();
+            System.out.println("Número da conta:");
+            int numero = Sc.nextInt();
+            System.out.println("Nome do cliente:");
+            String nomeCliente = Sc.next();
+            System.out.println("Saldo:");
+            double saldo = Sc.nextDouble();
+            
+            String texto = "Olá " + nomeCliente + " suas informações são: \n";
+            String info = " agência:" + agencia + "\n conta: " + numero + "\n saldo: " + saldo;
+            String resul = texto.concat(info);
+            System.out.println(resul);
+        }
     }
+
+
 }
